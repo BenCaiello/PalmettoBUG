ca7ef9488b049df6e1c9e50e76e8bf480532d378
Benjamin Caiello <138086695+BenCaiello@users.noreply.github.com>
2025-03-11 15:14:15 -0400

    try fix imc download stream

--- palmettobug/Entrypoint/app_and_entry.py
+++ palmettobug/Entrypoint/app_and_entry.py
@@ -701,9 +701,10 @@ def fetch_IMC_example(new_directory):
     if not os.path.exists(new_directory):
         raise Exception("Target directory does not exist")
     new_analyses_dir = new_directory + "/Analyses"
-    CyTOF_data = requests.get("https://zenodo.org/records/14983582/files/Example_IMC.zip?download=1")
+    IMC_data = requests.get("https://zenodo.org/records/14983582/files/Example_IMC.zip?download=1", stream = True)
     with open(new_directory + "/IMC_data.zip", 'wb') as write_to:
-        write_to.write(CyTOF_data.content)
+        for i in IMC_data.iter_content(chunk_size = 1024**2)
+            write_to.write(i)
     zip_archive = zipfile.ZipFile(new_directory + "/IMC_data.zip")
     zip_archive.extractall(new_directory)
 
