06ddadb393b3fe5836308166211a14d989004477
Benjamin Caiello <138086695+BenCaiello@users.noreply.github.com>
2025-04-07 14:29:19 -0400

    test GUI setup

--- palmettobug/Executable.py
+++ palmettobug/Executable.py
@@ -1,7 +1,6 @@
 ''' 
 This is a launching point for the GUI, that calls the main App class and initiates the GUI loop
 It also serves as a ``central`` place for modifying / filtering warnings & logging from the program
-
 '''
 
 ## Legend has it that PalmettoBUG stands for:
@@ -24,38 +23,13 @@ warnings.filterwarnings("ignore", message = "Setting an item of incompatible dty
 from .Entrypoint.app_and_entry import App # noqa: E402
 
 __all__ = ["run_GUI"]
-#from .Utils.sharedClasses import NapariProcessClass
 # import logging
 
 
 #### These final code blocks execute the program
 def run_GUI() -> None:
     '''Launches the PalmettoBUG GUI '''
-    #napari_launch = NapariProcessClass()
-    #napari_launch.start()
-    #napari_launch.getPipe()
     App1 = App(None)
-
-    '''
-    # no longer applicable / needed as cellpose (and before that rpy2) has been disconnected from the main program
-    class package_filterer(logging.Filter):      ## https://docs.python.org/3/howto/logging-cookbook.html#filters-contextual   
-                                                 # --> Example of using filters
-        def __init__(self, cellpose = False):
-            super().__init__()
-            self.cellpose = cellpose
-
-        def filter(self, record):
-            if (self.cellpose == True) & (record.levelno == 20):    ## block info messages from cellpose (message spam during normal operation)
-                return False
-
-    cellposesilencer1 = logging.getLogger("cellpose.models")
-    cellposesilencer2 = logging.getLogger("cellpose.core")
-    cellposesilencer3 = logging.getLogger("cellpose.denoise")
-    list_cellpose_loggers = [cellposesilencer1, cellposesilencer2, cellposesilencer3]
-
-    for i in list_cellpose_loggers:
-        i.addFilter(package_filterer(cellpose = True))
-    '''
     App1.mainloop()
 
 if __name__ == "__main__":
---
+++ tests/test_GUI.py
@@ -0,0 +1,5 @@
+import palmettobug
+from palmettobug.Entrypoint.app_and_entry import App
+
+def setup_app():
+    app = App(None)
