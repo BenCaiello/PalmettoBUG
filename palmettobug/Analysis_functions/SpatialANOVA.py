bc98051b7e73b6a19d0eb89fe1c89dfcbc9e7ee4
Benjamin Caiello <138086695+BenCaiello@users.noreply.github.com>
2025-08-08 09:59:59 -0400

    try to fix tests

--- palmettobug/Analysis_functions/SpatialANOVA.py
+++ palmettobug/Analysis_functions/SpatialANOVA.py
@@ -170,11 +170,11 @@ class SpatialANOVA():
             self.data_table['condition'] = list(self.exp.data.obs['condition'].astype('str'))
             self.data_table['sample_id']  = list(self.exp.data.obs['sample_id'].astype('str'))
             self.data_table['patient_id'] = list(self.exp.data.obs['patient_id'].astype('str'))
+            self.data_table.index = self.data_table.index.astype('str')
             if self.exp.back_up_data is not None:
                 self.filenames = self.exp.back_up_data.obs['file_name']
                 if self.cellType_key is not None:
                     self.data_table['cellType'] = 'dropped'
-                    self.data_table.index = self.data_table.index.astype('str')
                     self.data_table.loc[self.exp.data.obs.index,['cellType']] = list(self.exp.data.obs[self.cellType_key].astype('str'))
             elif self.cellType_key is not None:
                 self.data_table['cellType'] = list(self.exp.data.obs[self.cellType_key].astype('str'))
