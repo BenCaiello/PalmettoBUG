ebdb7f92768d52e8bb348dd621133dcb5dd84b20
Benjamin Caiello <138086695+BenCaiello@users.noreply.github.com>
2025-09-12 12:19:07 -0400

    expand GUI tests

--- tests/test_GUI.py
+++ tests/test_GUI.py
@@ -2,12 +2,16 @@ import os
 import palmettobug
 from palmettobug.Entrypoint.app_and_entry import App
 
+homedir = __file__.replace("\\","/")
+homedir = homedir[:(homedir.rfind("/"))]
+homedir = homedir[:(homedir.rfind("/"))]
 fetch_dir = homedir + "/project_folder"
 if not os.path.exists(fetch_dir):
     os.mkdir(fetch_dir)
 proj_directory = fetch_dir + "/Example_IMC"
 np.random.default_rng(42)
 
+##>>## GUI App & entrypoint tests
 def test_fetch_IMC():
     fetch_IMC_example(fetch_dir)
 
@@ -37,7 +41,16 @@ def test_img_entry_func():
 
 def test_FCS_choice():
     app.entrypoint.FCS_choice(fetch_dir + "/Example_CyTOF")
-    assert True   ## non-failure is enough for me right now, as it implies successful setting up of the widgets of the GUI
+    assert True  
+
+
+##>>## GUI Image Analysis tests
+def test_call_raw_to_img_part_1_hpf():
+    app.Tabs.image_proc_widg.call_raw_to_img_part_1_hpf()
+    assert True   
 
 def test_toggle_in_gui():
     palmettobug.ImageProcessing.ImageAnalysisClass.toggle_in_gui()   ## really here to reset --> not being in the gui after testing the App above
+
+
+    ##>>## GUI Image Analysis tests
